55803c3ec7416404a520c1c5c6727ca39e1889c2
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2019-04-10 14:48:37 -0400

    Updates

---
+++ precache-manifest.607bae3d325de058c98b8d4ee46543b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "62ac5bd5a1faa6c116fa",
+    "url": "/jeopardy/static/js/runtime~main.f29172a4.js"
+  },
+  {
+    "revision": "26d74c87397e1db26a9e",
+    "url": "/jeopardy/static/js/main.29d5526a.chunk.js"
+  },
+  {
+    "revision": "3e14d2223ca65cb44102",
+    "url": "/jeopardy/static/js/2.da5927f4.chunk.js"
+  },
+  {
+    "revision": "26d74c87397e1db26a9e",
+    "url": "/jeopardy/static/css/main.6bddf741.chunk.css"
+  },
+  {
+    "revision": "4c3d3a359deaffa5536e8e0581adb4a1",
+    "url": "/jeopardy/index.html"
+  }
+];
